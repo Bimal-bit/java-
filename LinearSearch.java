18ddfa6a0309ed477cacb71a3df99cd4e09ecac8
Bimal-bit <msilaptop737@gmail.com>
2024-11-04 23:36:53 +0530

    modified linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -12,3 +12,32 @@ public class LinearSearch {
         System.out.println("the value found at "+ count);
     }
 }
+
+
+
+//new code
+class serach1 {
+    public static void main(String[] args) {
+        int arr []={10,2,66,88,44,85,42};
+        int target = 44;
+        int ans=ls1(arr,target);
+        System.out.println(ans);
+    }
+}
+
+static int ls1(int []arr,int target){
+    if(arr.length==0){
+        return  -1;
+    }
+    for (int i = 0; i < arr.length; i++) {
+        int element = arr[i];
+        if (element==target){
+            return i;
+
+        }
+        else {
+            System.out.println("wrong input");
+
+
+    }
+}
