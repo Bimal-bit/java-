be92f0ab0ee92c9fe19bc3a27c1e60055608b70a
Bimal-bit <msilaptop737@gmail.com>
2024-11-08 23:35:34 +0530

    adding linear search

--- BinarySearch.java
+++ BinarySearch.java
@@ -27,7 +27,7 @@ class BinarySearch {
 }
 
 
-
+//
 class OrderDiagnosticBinarySearch {
     public static void main(String[] args) {
         int arr[] = {11, 12, 13, 14, 15, 16, 17, 18, 19};
@@ -55,5 +55,74 @@ class OrderDiagnosticBinarySearch {
         }
 
         return -1;
+    }
+}
+
+//ceiling of a number
+//Smallest number greater than equal to target
+class CeilingOfNumber {
+    public static void main(String[] args) {
+        int arr[] = {11, 12, 13, 14, 16, 17};
+        int target =18 ;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int search(int arr[], int target) {
+        if(target>arr.length-1){
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return start;
+    }
+}
+
+// reverse ceiling of a number
+//Smallest number lesser than equal to target
+
+class RverseCeilingOfNumber {
+    public static void main(String[] args) {
+        int arr[] = {9,11, 12, 13, 14, 16, 17};
+        int target =8 ;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int search(int arr[], int target) {
+        if(target<0){
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return end;
     }
 }
