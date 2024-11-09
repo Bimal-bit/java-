cd46434212627fd2ec622932f1b55f24fc920579
Bimal-bit <msilaptop737@gmail.com>
2024-11-09 11:57:41 +0530

    adding linear search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class BinarySearch {
     public static void main(String[] args) {
         int arr[] = {11, 12, 13, 14, 15, 16, 17, 18, 19};
@@ -153,4 +155,55 @@ class nextGreatestLetter{
 
         return arr[start % arr.length];
     }
+}
+
+
+//34. Find First and Last Position of Element in Sorted Array
+class FirstAndLast {
+    public static void main(String[] args) {
+        int arr[] = {5,7,7,7,7,8,8,8,9,10};
+        int target = 7;
+        int ans[] = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int nums[], int target) {
+        int []ans ={-1,-1};
+        int start=ok(nums,target,true);
+        int end =ok(nums,target,false);
+        ans[0]=start;
+        ans[1]=end;
+
+
+        return ans;
+    }
+
+
+    static int ok(int []arr,int target,boolean find){
+        int ans =-1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                ans=middle;
+                if(find==true){
+                    end=middle-1;
+                }
+                else {
+                    start=middle+1;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
 }
